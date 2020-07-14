5ec3645ecd2989e9fa2f318a4b8b5e68e2fb5e83
MRNIU <a6813140@hotmail.com>
2020-07-14 15:04:35 +0800

    Change  to 1000000. Add dev log

--- CM4/include/FreeRTOSConfig.h
+++ CM4/include/FreeRTOSConfig.h
@@ -67,7 +67,7 @@
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemD2Clock )
-#define configTICK_RATE_HZ                       ((TickType_t)1000)
+#define configTICK_RATE_HZ                       ((TickType_t)1000000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)15360)
